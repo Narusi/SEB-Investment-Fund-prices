c58301672bec54c5e28da4ad1e97092eaf8ebdc9
Nauris Krūmiņš <Nauris.krumins@gmail.com>
2019-02-18 17:04:05 +0200

    Update Analyzer.py

--- Analyzer.py
+++ Analyzer.py
@@ -1,8 +1,10 @@
-def getPortPrices(allocation):
-    import pandas as pd
+# Analyzing the gains of current plan
+import pandas as pd
+
+def getPortPrices(allocation):    
     
     print('\nReading fund data...')   
-    fundPrices = pd.read_excel('SEB funds.xlsx','DATA', index_col='DATE')
+    fundPrices = pd.read_csv('PRICES.csv', parse_dates = True, index_col = 'DATE')
     chosenFunds = fundPrices.filter(items=allocation.index)
     
     p = []
@@ -10,14 +12,13 @@ def getPortPrices(allocation):
     
     print('Calculating portfolio aggregated price...\n')
     for index, row in chosenFunds.iterrows():    
-        for i in isins:
-            p.append([pd.to_datetime(index), float(row[i]*allocation[i])])
+        for i in isins: p.append([pd.to_datetime(index), float(row[i]*allocation[i])]);
     
     portfolio = pd.DataFrame(p, columns=list(['DATES','PRICES']), dtype=float)
     pd.to_numeric(portfolio['PRICES'])
     pd.to_datetime(portfolio['DATES'], errors='coerce')
     
-    portfolio['DATES'].dropna
+    portfolio['DATES'].dropna()
     portfolio = portfolio.groupby('DATES')['PRICES'].sum()
     portGAINS = portfolio.pct_change().dropna()
     
@@ -27,33 +28,19 @@ def getPortPrices(allocation):
     
     return portfolio
 
-def getISINs():
-    import pandas as pd
-    
-    temp = pd.read_excel('SEB portfolio.xlsm', 'FUNDS', index_col='ISIN')
-    temp = temp.index.dropna()
-    isin = []
-    for index in temp:
-        isin.append(str(index))
-    return isin
+def run_allocator():
+	print('Getting aggregated portfolio prices:')
+	fundAlloc = pd.read_csv('FUND LIST.csv', index_col='Fund Name')
+	fundAlloc = fundAlloc[['ISIN']]
+	fundAlloc['ALLOCATION'] = 1/len(fundAlloc['ISIN'])
 
-import pandas as pd
-import matplotlib.pyplot as plt
+	pAlloc = fundAlloc.filter(items=['ISIN', 'ALLOCATION']).loc[(fundAlloc['ALLOCATION'] != 0.00) & (fundAlloc.index != 'Total')].groupby('ISIN')['ALLOCATION'].sum()
+	x = getPortPrices(pAlloc).pct_change()*100
 
-print('Getting aggregated portfolio prices:')
-fundAlloc = pd.read_excel('SEB portfolio.xlsm', 'FUNDS', index_col='Fund Name', dtype={'ISIN':str, 'ALLOCATION':float})
+	isin = fundAlloc['ISIN']
+	aBench = pd.DataFrame({'ISIN':isin, 'ALLOCATION':round(1/len(isin), 3)}).groupby('ISIN')['ALLOCATION'].sum()
+	xx = getPortPrices(aBench).pct_change()*100
 
-pAlloc = fundAlloc.filter(items=['ISIN', 'ALLOCATION']).loc[(fundAlloc['ALLOCATION'] != 0.00) & (fundAlloc.index != 'Total')].groupby('ISIN')['ALLOCATION'].sum()
-x = getPortPrices(pAlloc).pct_change()*100
-
-isin = getISINs()
-aBench = pd.DataFrame({'ISIN':isin, 'ALLOCATION':round(1/len(isin), 3)}).groupby('ISIN')['ALLOCATION'].sum()
-xx = getPortPrices(aBench).pct_change()*100
-
-comparison = pd.DataFrame({'PORTFOLIO':x.rolling(60).mean(), 'SEB BENCH':xx.rolling(60).mean(), 'CORR Portfolio vs SEB BENCH':x.rolling(60).corr(xx)}, index=x.index)
-
-plt.plot(comparison)
-plt.ylabel('Gains/Loses')
-plt.xlabel('Dates')
-plt.legend(comparison.columns)
-plt.show()
+	comparison = pd.DataFrame({'PORTFOLIO':x.rolling(60).mean(), 'SEB BENCH':xx.rolling(60).mean(), 'CORR Portfolio vs SEB BENCH':x.rolling(60).corr(xx)}, index=x.index)
+	
+	return comparison
